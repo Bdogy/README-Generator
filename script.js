9e31afbe3ffc2aa623bbf10762b51d4713099d5b
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-18 14:46:40 -0700

    added all questions for the user

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ inquirer
   .prompt([
     {
       type: "input",
+      message: "Whats your user name?",
+      name: "userName",
+    },
+    {
+      type: "input",
       message: "whats the Project name?",
       name: "name",
     },
@@ -15,10 +20,26 @@ inquirer
     },
     {
       type: "input",
+      message: "What are the install instructions",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "What are is the usage?",
+      name: "use",
+    },
+    {
+      type: "input",
+      message: "Who also contributed?",
+      name: "contribute",
+    },
+    {
+      type: "list",
       message: "Whats your license?",
+      choices: ["1", "2", "3"],
       name: "license",
     },
   ])
   .then((response) => {
-    // appendToHtml(response);
+    console.log(response);
   });
